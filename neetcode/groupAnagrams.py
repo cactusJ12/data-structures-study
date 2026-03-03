1f56ed7cd25ae7bda2ad2944a81e722047db515f
Sun Jang <jsunwoong23@g.ucla.edu>
2026-03-02 23:07:37 -0800

    topKFrequent.py

--- neetcode/groupAnagrams.py
+++ neetcode/groupAnagrams.py
@@ -2,8 +2,17 @@ from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
-        
+        h = defaultdict(list)
 
+        for s in strs:
+            key = [0] * 26
+
+            for c in s:
+               key[ord(c) - ord("a")] += 1
+
+            h[tuple(key)].append(s)        
+
+        return list(h.values())
         
 
 """
